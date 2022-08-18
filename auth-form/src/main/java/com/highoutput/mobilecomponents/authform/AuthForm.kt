fd15a80b9689d184cb4af3d39b189a38fe311243
Ralph Ordanza <mendelordanza@gmail.com>
2022-08-18 11:45:14 +0800

    build: remove preview from auth-form

--- auth-form/src/main/java/com/highoutput/mobilecomponents/authform/AuthForm.kt
+++ auth-form/src/main/java/com/highoutput/mobilecomponents/authform/AuthForm.kt
@@ -1,24 +1,16 @@
 package com.highoutput.mobilecomponents.authform
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.Icon
 import androidx.compose.material.Text
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Lock
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.highoutput.mobilecomponents.coreui.CustomTextbox
-import com.highoutput.mobilecomponents.coreui.DropdownField
 import com.highoutput.mobilecomponents.coreui.SingleTapButton
 
 @Composable
@@ -97,19 +89,5 @@ fun AuthForm(
             )
         }
     }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    AuthForm(
-        modifier = Modifier.padding(24.dp),
-        onLoginClick = { email, pass ->
-
-        },
-        onSignUpClick = {
-
-        },
-    )
 }
 
